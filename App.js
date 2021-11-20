b8ab41b0331c8403c5c321e1b400e9e7a2c36d4f
Ale Flores <alefloresvera04@gmail.com>
2021-11-20 14:32:35 -0300

    Creción y primeras palabras del chatbot

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Esto será un chatbot</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const botAvatar = require('./assets/t_a16e6647052e49739c12419019c64d13_name_asfsa.jpg')
+
+const BOT = {
+  _id: 2,
+  name: 'Sr. Bot',
+  avatar: botAvatar
+}
+class App extends Component {
+
+  state = {
+    messages: [{_id: 2, text: 'Soy el Sr. Bot', createdAt: new Date(), user: BOT}, {_id: 1, text: 'Hola', createdAt: new Date(), user: BOT}],
+    id: 1,
+    name: ''
+  }
+
+  onSend(messages = []){
+
+  }
+
+  onQuickReply(quickReply){
+    
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(message) => this.onSend(message)}
+          onQuickReply={(quickReply) => this.onQuickReply(quickReply)}
+          user={{ _id: 1 }} />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
