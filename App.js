5f345ef07b8b7b85ee18792d8a863e12d3d7a53a
Ale Flores <alefloresvera04@gmail.com>
2021-11-22 20:35:56 -0300

    Implementación de dialogflow y elementos de react

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Card, Button } from 'react-native-elements';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import { Dialogflow_V2 } from 'react-native-dialogflow';
+
+import { dialogflowConfig } from './env';
 
 const botAvatar = require('./assets/t_a16e6647052e49739c12419019c64d13_name_asfsa.jpg')
 
@@ -13,18 +17,155 @@ const BOT = {
 class App extends Component {
 
   state = {
-    messages: [{_id: 2, text: 'Soy el Sr. Bot', createdAt: new Date(), user: BOT}, {_id: 1, text: 'Hola', createdAt: new Date(), user: BOT}],
+    messages: [{ _id: 2, text: 'Soy el Sr. Bot', createdAt: new Date(), user: BOT }, { _id: 1, text: 'Hola', createdAt: new Date(), user: BOT }],
     id: 1,
     name: ''
   }
 
-  onSend(messages = []){
+  componentDidMount() {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_SPANISH,
+      dialogflowConfig.project_id,
+    );
+  }
+
+  handleGoogleResponse(result) {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+
+    this.sendBotResponse(text);
+  }
+
+  sendBotResponse(text) {
+    let msg;
+
+    if (text == 'viajar') {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text: 'Te gustaría conocer la perla del sur?',
+        image: 'https://infonegocios.com.py/uploads/encartacion-404anios-plus-marzo3.jpg',
+        createdAt: new Date(),
+        user: BOT,
+      };
+    } else if (text == 'mostrar opciones') {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text: 'Eliga su bus',
+        createdAt: new Date(),
+        user: BOT,
+        isOptions: true,
+        data: [
+          {
+            title: 'El Tigre',
+            image: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQcbtVRAeDYiTytsgTsbBRMlOgW1geB5p6fbD8TYhBp_Sk-aor2',
+          },
+          {
+            title: 'Beato',
+            image: 'https://www.disfrutandoparaguay.com/data/article/images/95/beato.jpg',
+          },
+          {
+            title: 'NSA',
+            image: 'https://www.ticketonline.com.ar/assets/img/empresas-pages/nuestra-senora-de-la-asuncion/nuestra-senora-de-la-asuncion-1.jpg',
+          },
+        ],
+      };
+      /*msg = {
+        _id: this.state.messages.length + 1,
+        text: 'Aquí puedes seleccionar con que colectivo viajar!',
+        createdAt: new Date(),
+        user: BOT,
+        quickReplies: {
+          type: 'radio',
+          keepIt: true,
+          values: [
+            {title: 'El Tigre', value: 'El tigre', bColor: '#A0522D', bgColor: '#A0522D'},
+            {title: 'Beato', value: 'Beato', bColor: '#A0522D', bgColor: '#A0522D'},
+            {title: 'NSA', value: 'NSA', bColor: '#A0522D', bgColor: '#A0522D'},
+          ],
+        },
+      };*/
+    } else {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        user: BOT,
+      };
+    }
+
+    this.setState((previouseState) => ({
+      messages: GiftedChat.append(previouseState.messages, [msg]),
+    }));
 
   }
 
-  onQuickReply(quickReply){
-    
+  onSend(messages = []) {
+    this.setState((previouseState) => ({
+      messages: GiftedChat.append(previouseState.messages, messages)
+    }))
+
+    let message = messages[0].text;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error)
+    )
   }
+
+  onQuickReply(quickReply) {
+    this.setState((previouseState) => ({
+      messages: GiftedChat.append(previouseState.messages, quickReply)
+    }))
+
+    let message = quickReply[0].value;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => this.handleGoogleResponse(result),
+      (error) => console.log(error)
+    )
+  }
+
+  renderBubble = props => {
+
+    if (props.currentMessage.isOptions) {
+      return (
+        <ScrollView>
+          {props.currentMessage.data.map((item) => (
+            <Card
+              containerStyle={{
+                padding: 0,
+                borderRadius: 15, paddingBottom: 7, overflow: 'hidden',
+              }}
+              key={item.title}>
+              <Card.Image style={{ width: 220, height: 110 }} resizeMode="cover" source={{ uri: item.image }}></Card.Image>
+              <Card.Divider />
+              <Card.Title>{item.title}</Card.Title>
+              <Button
+                title="Escoger"
+                style={{ height: 35 }}
+                onPress={() => this.sendBotResponse
+                  (item.title)}
+              />
+            </Card>
+          ))}
+        </ScrollView>
+      )
+    }
+
+    return (
+      <Bubble
+        {...props}
+        textStyle={{ right: { color: 'white' } }}
+        wrapperStyle={{
+          left: { backgroundColor: 'gray' },
+          right: { backgroundColor: 'blue' },
+        }}
+      />
+    );
+  };
 
   render() {
     return (
@@ -33,6 +174,7 @@ class App extends Component {
           messages={this.state.messages}
           onSend={(message) => this.onSend(message)}
           onQuickReply={(quickReply) => this.onQuickReply(quickReply)}
+          renderBubble={this.renderBubble}
           user={{ _id: 1 }} />
       </View>
     );
---
+++ env.js
@@ -0,0 +1,13 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "chatbot-dnpy",
+    "private_key_id": "0b26b685b3fcabe38d8787246218059d83ce72a0",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCcAfzrniFu6K1g\nfdHYSng/UTDnib1e387sHZ/OthGnrEec4+y356kovAf4N0p2aUufc1YO34bOCaI1\nKIX9U3lfB1DIKLRj+iHW0MUdUowWwHiSPbnEEwMJfVL48wxth8fHYgdHnV5smbV7\n2RcHf2O5s+ymsQuhTm8HkELvm9J9Y18hqBF8q0jj1E2SU6AohOADLm5wRRPEaV9s\nMUoVqkcPPZ1OO2XYkxvXwt52Nbc5LDDkN/8SPHvXykbHAbhWAgz9VmPiLYjPe+WS\nHA+1L76hrafoopXM9PJBUbQAHXKptcE0nCSc0zKlpK3QS1u9gl2taFVTgaFjT5lJ\nTBdSlGnLAgMBAAECggEAMyTfsqqzkTWMaT62Ra8cPr2yR6W7RFgg8P1BP7YmkY6E\nJ6X9G8lBho4mGrSdn7YjSZG5G8Uh33f/CmhPjN4JwyKV1BisEr6Uy2TDyjoenPLa\nVC8SMHp/G2RPiDe90kaaMv4rHsozg2zct05TI20a121p3rfZ9TbOZW9g0kaGOf6P\nghsqOFXvL2RiiMtkGUuQfdHtWwwBrBLcAYWCFcAaiENDCXR9xI7GSxmcjvzJ1vag\nN99LoT+Kg1ngHiIEnT3YlEH8WA7qHpAUAkHMKhUnSffrUy403LML5MaNHEkjrgEL\no1STFraXae1wGAMI7cTvom9Z7+TBZ+CItEdZbMgJ6QKBgQDV8wcJNSeBdT6BAOUJ\ntYsMAn9E+pLkzGmY4Z8IOdaIURdMdcH1pR0lTkF5Lv9vfThhCgZdH+tUNSegel+C\np9eBv6UrSQmEFoI6uUALkGS6924F8aTqMOXyPW+Thj5BZyzyX8mP8VzG2Pztr+l8\nhd1LNHSaDkSCJktd/OBtq5fuNwKBgQC6q5m5cRv0dLoFv1etBTppWzMfJ0DcSZ/b\nRPTA9L1gcNiBYUR77NIL0b+9kDZ/KsVdLHNvTNLR4dpucLw/Y2FeSIpVlmvCH1A2\nlevxINtw6jUVirvx8lmneYqfUPazoqJuGTnJNnhcnPjYf/QMpxcSpZMzilzuqgGj\nm3Eu2gy3DQKBgQCBGabzPWnbSl4vBdUvKKcA1jjp7LHa8iX+JJcMJap4k0YV0zD5\nA2s06aXEdEGaitsMYzo301zzcSe6RF2Bv7nhBz2pgucFF3f563LkB3tg/pEbcRbs\nYhk/NScww96pJJMQphBAlYpASwtEedPQWg6QpNR/aYCZcHYl/sOGKfcBvQKBgQCK\nL6wd5kief6aRyKLb94nVImlGv6U5LduD05koGkTy1ra/mES9RIP9sekyKEbffsca\nAWT22LUcIAi2RwhkGGqlBCg70Z21175mmjdOildIUGi5qLkXE6Qg4U2oQKRQOK3r\nuQrZFFSnoqBojNgaYbsn16CPv/apyvppphF0cICHJQKBgF6ru1eLMCk4JCzeZb/s\nexXymmJcW5fV9saZKqsL+Hz8WtCaa1MBE83si7/LY48/iR0+Ft1N+lQKnGcpDIBP\nEPEBOsnkIpYZsEpiXo204K/owWVcjoB7vxTQQV4EpGW1PkWL/6rxWGpfp3jLDAwQ\n9Uu6oRzlZetRmReNWzIjW/Il\n-----END PRIVATE KEY-----\n",
+    "client_email": "chatbot-escucha@chatbot-dnpy.iam.gserviceaccount.com",
+    "client_id": "106845524236963204399",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chatbot-escucha%40chatbot-dnpy.iam.gserviceaccount.com"
+  }
+  
